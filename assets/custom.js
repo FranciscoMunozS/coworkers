d21623f7a5a554dbd090dcf523649eb1b3a25f54
Francisco <franciscomuniozs@gmail.com>
2019-03-13 09:40:39 -0300

    first commit

---
+++ assets/custom.js
@@ -0,0 +1,7 @@
+// Cerrar mensajes
+
+$(document).ready(function() {
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
+});
---
+++ scripts.58ded34a11135cb25a83.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".message .close").on("click",function(){$(this).closest(".message").transition("fade")})});
